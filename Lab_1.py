8effddde8a0cb7fe4c1cbca1300ae26c8bbc3dc3
Kohan Danila 953503 <ew1011735@gmail.com>
2021-03-06 17:23:51 +0300

    created new branch

--- Lab_1.py
+++ Lab_1.py
@@ -1,10 +1,18 @@
-c = [[1/5, 0, 0.2, 0, 0], [0, 0.2, 0, 0.2, 0], [0.2, 0, 0.2, 0, 0.2], [0, 0.2, 0, 0.2, 0], [0, 0, 0.2, 0, 0.2]]
-k=12
+import logging
 
-for row in c:
-	for index, elem in enumerate(row):
-		print(elem, k, round(elem*k, 1))
-		row[index]=round(elem*k, 1)
+logging.basicConfig(level=logging.INFO)
 
-print(c)
-print(c[0][2])
+def main():
+	c = [[1/5, 0, 0.2, 0, 0], [0, 0.2, 0, 0.2, 0], [0.2, 0, 0.2, 0, 0.2], [0, 0.2, 0, 0.2, 0], [0, 0, 0.2, 0, 0.2]]
+	k=12
+
+	for row in c:
+		for index, elem in enumerate(row):
+			print(elem, k, round(elem*k, 1))
+			row[index]=round(elem*k, 1)
+
+	logging.info(c)
+	logging.info(c[0][2])
+
+if __name__ == "__main__":
+	main()
