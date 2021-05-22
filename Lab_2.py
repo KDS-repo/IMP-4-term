dbe10a1a218eaff0dd1162adbafd6c2a04a89868
Kohan Danila 953503 <ew1011735@gmail.com>
2021-05-22 11:35:55 +0300

    Made function work non-statically

--- Lab_2.py
+++ Lab_2.py
@@ -1,11 +1,5 @@
 import pickle
 import types
-
-class Attempt(object):
-    def func():
-        print("Hello")
-    field = 45
-    field2 = 4
  
 def create_function(name, args):
     def y(): pass
@@ -23,6 +17,12 @@ def create_function(name, args):
                 y.func_code.co_lnotab)
     return types.FunctionType(y_code, y.func_globals, name)
 
+class Attempt(object):
+    def func():
+        print("Hello")
+    field = 45
+    field2 = 4
+
 def main():
 #    attempt = 35
 #    output = pickle.dumps(attempt)
@@ -38,19 +38,40 @@ def main():
     #deleting instance field, revealing class attribute
     del a.field
     print(Attempt.field)
+    print(Attempt.func)
     print(Attempt.__dict__)
     print(a.field)
     print(hasattr(a, "field"))
     
+    exec(Attempt.func.__code__)
+    
+    def newfunc():
+        pass
+    
+    #newfunc = type(Attempt.func.__name__, (), Attempt.func.__dict__)
+    #newfunc()
+    #type(...) doesn't work like that, if it did, it would print
+    #proof - b = newfunc later
+    print(type(Attempt.func))
     print(dir(Attempt.func))
-    for part in dir(Attempt.func):
-        print(part)
-        print(hasattr(Attempt.func, part))
-        if(hasattr(Attempt.func, part)):
-            print(getattr(Attempt.func, part))
-            
-    f_dict = dir(Attempt.func)
-    a = create_function("FuncName", f_dict)
+    a = getattr(Attempt.func, "__code__")
+    print("code\n", a)
+    setattr(newfunc, "__code__", a)
+    #for part in dir(Attempt.func):
+        #print(part)
+        #print(hasattr(Attempt.func, part))
+        #if(hasattr(Attempt.func, part)):
+            #a = getattr(Attempt.func, part)
+            #print(a)
+            #setattr(newfunc, part, a)
+        
+    print(newfunc)
+    newfunc()
+    b = newfunc
+    b()
+    
+    #f_dict = dir(Attempt.func)
+    #a = create_function("FuncName", f_dict)
     
     
     
