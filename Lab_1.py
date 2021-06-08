5998be6448aeac64a35a81e01feab9e00b0b2b15
Kohan Danila 953503 <ew1011735@gmail.com>
2021-06-09 00:22:58 +0300

    Refined Lab_1 code ot standard

--- Lab_1.py
+++ Lab_1.py
@@ -1,10 +1,25 @@
-c = [[1/5, 0, 0.2, 0, 0], [0, 0.2, 0, 0.2, 0], [0.2, 0, 0.2, 0, 0.2], [0, 0.2, 0, 0.2, 0], [0, 0, 0.2, 0, 0.2]]
-k=12
+import logging
 
-for row in c:
-	for index, elem in enumerate(row):
-		print(elem, k, round(elem*k, 1))
-		row[index]=round(elem*k, 1)
+logging.basicConfig(level=logging.INFO)
 
-print(c)
-print(c[0][2])
+
+def main():
+    c = [[], [], [], [], []]
+    c[0] = [0.2, 0, 0.2, 0, 0]
+    c[1] = [0, 0.2, 0, 0.2, 0]
+    c[2] = [0.2, 0, 0.2, 0, 0.2]
+    c[3] = [0, 0.2, 0, 0.2, 0]
+    c[4] = [0, 0, 0.2, 0, 0.2]
+    k = 12
+
+    for row in c:
+        for index, elem in enumerate(row):
+            logging.info(str(elem) + " " + str(k) + " " + str(round(elem*k, 1)))
+            row[index] = round(elem*k, 1)
+
+    logging.info(c)
+    logging.info(c[0][2])
+
+
+if __name__ == "__main__":
+    main()
