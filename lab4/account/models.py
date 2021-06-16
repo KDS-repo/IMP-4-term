da52c204f0e994f58146141f44873ab2a7720fc5
Kohan Danila 953503 <ew1011735@gmail.com>
2021-06-16 18:24:21 +0300

    Started work on personal lab4. Error in concurrent.futures

---
+++ lab4/account/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+from django.conf import settings
+
+from concurrent.futures import ThreadPoolExecutor
+
+class Profile(models.Model):
+    f1 = ThreadPoolExecutor().submit(models.OneToOneField)
+    user = f1.result(settings.AUTH_USER_MODEL)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
+
+    def __str__(self):
+        return 'Profile for user {}'.format(self.user.username)
